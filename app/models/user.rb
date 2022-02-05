197f8d247f884d33f8b3da7cc7c29ddd9bc285e8
Jamie <lijamie19828@gmail.com>
2022-02-04 23:20:48 -0800

    add basic validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates_length_of :username, within: 4..12
+
+  validates :email, presence: true, uniqueness: true
+
+  validates :password, presence: true
+  validates_length_of :password, within: 6..16
+
+  has_many :posts, dependent: :destroy
 end
