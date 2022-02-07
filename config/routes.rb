492d1914a0c9af9ceff6478a4276cc516dd0a152
Jamie <lijamie19828@gmail.com>
2022-02-07 12:42:01 -0800

    create show view, update routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   Rails.application.routes.draw do
     root 'posts#index'
-    get '/posts', to: 'posts#index'
+    
+    resources :posts
   end
 
 end
